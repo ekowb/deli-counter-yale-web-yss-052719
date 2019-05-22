eb25f4c7756f48605101ca9f1649e86b3b7d724a
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-22 18:03:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,9 @@
 # Write your code here.
 def line(array)
   if array.empty? == true
-    puts "The line is currently empty." 
+    puts "The line is currently empty."
+  else
+    return array
+  end
+end
+      
